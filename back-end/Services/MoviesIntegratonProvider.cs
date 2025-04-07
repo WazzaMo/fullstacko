0ef8c025d3fcbc4549f559901e8e567a615e8b07
Warwick Molloy <wazzam@lokeldigital.com>
2025-04-08 07:12:43 +1000

    Removed debug logging of api-token and improved api-token management; Refactored code to allow for time-out backoff and to centralise the way the provider API endpoints are called.

--- back-end/Services/MoviesIntegratonProvider.cs
+++ back-end/Services/MoviesIntegratonProvider.cs
@@ -9,50 +9,44 @@ public class MoviesIntegrationProvider : IIntegrationProvider
     private readonly HttpClient _httpClient;
     private const string BASE_URL = "https://webjetapitest.azurewebsites.net/api";
     private string API_TOKEN = "GOES-HERE";
-    private readonly IConfiguration _configuration;
+    private readonly IConfiguration _Configuration;
     
     const double TIMEOUT = 1.5d;
+    private double _TimeoutDuration = TIMEOUT;
+    private readonly TimeSpan _Timeout = TimeSpan.FromSeconds(TIMEOUT);
 
     public MoviesIntegrationProvider(HttpClient httpClient, IConfiguration configuration)
     {
         _httpClient = httpClient;
-        _configuration = configuration;
-        API_TOKEN = _configuration.GetValue<string>("API_TOKEN") ?? "GOES-HERE";
-        Console.WriteLine($"API_TOKEN: {API_TOKEN}");
+        _Configuration = configuration;
+        var apiToken = _Configuration.GetValue<string>("API_TOKEN");
+        if (apiToken == null)
+        {
+            Console.Error.WriteLine("API_TOKEN is not configured");
+        }
+        else
+        {
+            API_TOKEN = apiToken;
+        }
         _httpClient.DefaultRequestHeaders.Add("x-access-token", API_TOKEN);
     }
 
     public async Task<IEnumerable<MovieSummaryModel>> GetMovies()
     {
-        List<MovieSummaryModel> allMovies = [];
-        TimeSpan timeout = TimeSpan.FromSeconds(TIMEOUT);
-
+        string endpoint1 = $"{BASE_URL}/filmworld/movies";
+        string endpoint2 = $"{BASE_URL}/cinemaworld/movies";
+        string[] endpoints = ["/filmworld/movies", "/cinemaworld/movies"];
+        MovieListModel movieList = new MovieListModel();
         try
         {
-            var filmWorldTask = _httpClient.GetAsync($"{BASE_URL}/filmworld/movies");
-            var cinemaWorldTask = _httpClient.GetAsync($"{BASE_URL}/cinemaworld/movies");
-            Task<HttpResponseMessage>[] getTasks = [filmWorldTask, cinemaWorldTask];
-
-            int index = Task.WaitAny(getTasks, timeout);
-            if (index == -1)
-            {
-                throw new Exception("Timeout while fetching movies");
-            }
-
-            Task<HttpResponseMessage> getResponse = getTasks[index];
-            var response = await getResponse.Result.Content.ReadAsStringAsync();
-            if (response != null)
-            {
-                var movieList = JsonSerializer.Deserialize<MovieListModel>(response) ?? new MovieListModel();
-                allMovies = movieList.Movies;
-            }
+            movieList = await GetData<MovieListModel>(endpoint1, endpoint2);
         }
         catch (Exception ex)
         {
             throw new Exception("Error fetching movies", ex);
         }
 
-        return allMovies;
+        return movieList.Movies;
     }
 
     public async Task<MovieDetailsModel> GetMovieDetails(string movieId)
@@ -64,28 +58,50 @@ public class MoviesIntegrationProvider : IIntegrationProvider
 
         try
         {
-            var cinemaWorldTask = _httpClient.GetAsync(CinemaWorldEndpoint);
-            var filmWorldTask = _httpClient.GetAsync(FilmWorldEndpoint);
-            Task<HttpResponseMessage>[] getTasks = [cinemaWorldTask, filmWorldTask];
-            TimeSpan timeout = TimeSpan.FromSeconds(1.5d);
-
-            int index = Task.WaitAny(getTasks, timeout);
-            if (index == -1)
-            {
-                throw new Exception("Timeout while fetching movie details");
-            }
-            Task<HttpResponseMessage> getResponse = getTasks[index];
-            var response = await getResponse.Result.Content.ReadAsStringAsync();
-            if (response != null)
-            {
-                movieDetails = JsonSerializer.Deserialize<MovieDetailsModel>(response) ?? new MovieDetailsModel();
-            }
-
-            return movieDetails;
+            movieDetails = await GetData<MovieDetailsModel>(CinemaWorldEndpoint, FilmWorldEndpoint);
         }
         catch (Exception ex)
         {
             throw new Exception("Error fetching movie details", ex);
+        }
+
+        return movieDetails;
+    }
+
+    private async Task<T> GetData<T>(string endpoint1, string endpoint2)
+    {
+        string[] endpoints = [endpoint1, endpoint2];
+
+        Task<HttpResponseMessage> response1 = _httpClient.GetAsync(endpoint1);
+        Task<HttpResponseMessage> response2 = _httpClient.GetAsync(endpoint2);
+        Task<HttpResponseMessage>[] getTasks = [response1, response2];
+        int index = Task.WaitAny(getTasks, GetTimeout());
+        if (index == -1)
+        {
+            BackoffTimeout();
+            throw new Exception("Timeout while fetching movie details");
+        }
+        Console.WriteLine($"Response returned from : {endpoints[index]}");
+
+        Task<HttpResponseMessage> getResponse = getTasks[index];
+        var responseContent = await getResponse.Result.Content.ReadAsStringAsync();
+        
+        T result = JsonSerializer.Deserialize<T>(responseContent)
+                ?? throw new Exception("Failed to deserialize response");
+        return result;
+    }
+
+    private TimeSpan GetTimeout()
+    {
+        return TimeSpan.FromSeconds(_TimeoutDuration);
+    }
+
+    private void BackoffTimeout()
+    {
+        if (_TimeoutDuration < 60)
+        {
+            _TimeoutDuration *= 2;
+            Console.WriteLine($"Timeout duration increased to {_TimeoutDuration} seconds");
         }
     }
 }
