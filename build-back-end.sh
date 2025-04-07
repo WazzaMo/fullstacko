231f51e79924dc000c8d00ca4be99bc39080a3ef
Warwick Molloy <wazzam@lokeldigital.com>
2025-04-08 07:45:16 +1000

    Improving docker setup

--- build-back-end.sh
+++ build-back-end.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-docker build -t localhost:6000/movieapi -f back-end/dotnetDockerfile --network=host back-end/
+# docker buildx build --add-host=host.docker.internal:host-gateway -t localhost:6000/movieapi -f back-end/dotnetDockerfile --network=host back-end/
+docker buildx build --allow=network.host -t localhost:6000/movieapi -f back-end/dotnetDockerfile --network=host back-end/
 
 docker push localhost:6000/movieapi
